d642fce154552f9bfde70bd9f19e45bfab7c1a74
Ed-Games <edmarcosfilho2015@gmail.com>
2020-10-14 19:04:37 -0300

    update: modules added to app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,19 @@
-import express, { response } from 'express'
-import connection from './database/connection'
+import express from 'express'
+import 'express-async-errors'
+import './database/connection'
+import routes from './routes'
+import path from 'path'
+import errorHandler from './errors/handler'
+import  cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-
-app.get('/user', (request, response)=>{
-    console.log('sgssdeghsioghwiog')
-    return response.json({message: 'NLW3'})
-})
+app.use(routes)
+app.use('/uploads',express.static(path.join(__dirname,'uploads')))
+console.log(path.join(__dirname))
+app.use(errorHandler)
 
 app.listen(3333)
 
